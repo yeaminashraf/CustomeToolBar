313da7d6ea278214d8ecab7f71df7c1f14cb9f2e
yeaminashraf <40772180+yeaminashraf@users.noreply.github.com>
2018-10-01 00:03:55 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mirja/custometoolbar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mirja.custometoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((android.support.v7.widget.Toolbar) findViewById(R.id.action_bar));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
